fb92728da4c67da808dfb6d3fc24bcef7ac8d91a
Romulo Vieira <romulo.vieira777@gmail.com>
2024-07-23 10:14:38 -0300

    Python Fundamentals

---
+++ Section_02_Python_Fundamentals/Python_Code/02_Python_Introduction.py
@@ -0,0 +1,17 @@
+print("Hello World!")
+
+
+def func1():
+    print("Python is a fucntional programming language")
+
+
+func1()
+
+
+class aviral:
+    def func2(self):
+        print("Python is a object oriented programming language")
+
+
+obj = aviral()
+obj.func2()
---
+++ Section_02_Python_Fundamentals/Python_Code/03_Python_Type.py
@@ -0,0 +1,8 @@
+a = 1
+print(type(a))
+
+b = 1.0
+print(type(b))
+
+c = "Hello"
+print(type(c))
