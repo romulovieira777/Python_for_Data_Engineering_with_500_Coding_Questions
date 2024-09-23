9bd1de2fffc3d750eefa96aa602cc8ce2eacd20e
Romulo Vieira <romulo.vieira777@gmail.com>
2024-09-23 09:33:57 -0300

    Datatypes in Python

---
+++ Section_03_Datatypes_in_Python/Python_Code/01_Data_Types.py
@@ -0,0 +1,67 @@
+a = 10
+
+print(a)
+
+print('#############################################')
+
+aviral = 10
+Aviral = 30
+AVIRAL = 50
+
+print(aviral, Aviral, AVIRAL)
+
+print('#############################################')
+
+Aviral123 = 90
+
+print(Aviral123)
+
+print('#############################################')
+
+# Inavalid Syntax
+# 123Aviral = 90
+
+# print(123Aviral)
+
+# print('#############################################')
+
+_aviral = 100
+
+print(_aviral)
+
+print('#############################################')
+
+import keyword
+
+print(keyword.kwlist)
+
+print('#############################################')
+
+# Inavalid Syntax
+# None = 10
+
+# print(None)
+
+# print('#############################################')
+
+# Inavalid Syntax
+
+# avi$ral = 10
+
+# print(avi$ral)
+
+# print('#############################################')
+
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx = 1000
+
+print(xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx)
+
+print('#############################################')
+
+_aviral = 100
+__aviral = 200
+__aviral_ = 300
+
+print(_aviral, __aviral, __aviral_)
+
+print('#############################################')
